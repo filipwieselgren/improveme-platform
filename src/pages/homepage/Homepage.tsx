21a7902632848ea7a85b9a176b7c3558dac41da4
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2023-01-03 13:16:38 +0100

    main

--- src/pages/homepage/Homepage.tsx
+++ src/pages/homepage/Homepage.tsx
@@ -1,9 +1,11 @@
+import Main from "./Main";
 import Navbar from "./Navbar";
 
 const Homepage = () => {
   return (
     <>
       <Navbar />
+      <Main />
     </>
   );
 };
---
+++ src/pages/homepage/Main.tsx
@@ -0,0 +1,40 @@
+import fr from "../../assets/fr.png";
+import bee from "../../assets/bee.png";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <div className="main-wrapper">
+        <h1 className="txt-200 main-h1">
+          Take controll over your user feedback{" "}
+          <span className="line-h1 txt-400">|</span> develop better products
+        </h1>
+        <section className="section-one">
+          Make it easy for your users to provide your development team with
+        </section>
+        <section className="section-two">
+          <div className="errend-wrapper">
+            <div className="circle circle-green"></div>
+            <div className="errend-txt">Feature requests</div>
+            <img src={fr} alt="feature request" className="errend-img" />
+          </div>
+          <div className="errend-wrapper">
+            <div className="circle circle-red"></div>
+            <div className="errend-txt">Bug reports</div>
+            <img src={bee} alt="feature request" className="errend-img" />
+          </div>
+          <div className="errend-wrapper">
+            <div className="circle circle-yellow"></div>
+            <div className="errend-txt">General improvements</div>
+            <img src={fr} alt="feature request" className="errend-img" />
+          </div>
+        </section>
+        <section className="section-three">
+          so you can develop the best product.
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
