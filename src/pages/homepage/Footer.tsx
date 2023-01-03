ab4f55e3853af12174abc7c6b8f31e8c75392045
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2023-01-03 14:40:54 +0100

    icons

--- src/pages/homepage/Footer.tsx
+++ src/pages/homepage/Footer.tsx
@@ -1,4 +1,7 @@
 import logo from "../../assets/improveMeLogo.png";
+import { FiInstagram } from "react-icons/fi";
+import { AiOutlineFacebook } from "react-icons/ai";
+import { TfiTwitter } from "react-icons/tfi";
 
 const Footer = () => {
   return (
@@ -49,13 +52,19 @@ const Footer = () => {
             <h3>Follow Us</h3>
             <ul className="some">
               <a href="#">
-                <li>IG</li>
+                <li>
+                  <FiInstagram className="react-icon" />
+                </li>
               </a>
               <a href="#">
-                <li>FB</li>
+                <li>
+                  <AiOutlineFacebook className="react-icon" />
+                </li>
               </a>
               <a href="#">
-                <li>TW</li>
+                <li>
+                  <TfiTwitter className="react-icon" />
+                </li>
               </a>
             </ul>
           </div>
