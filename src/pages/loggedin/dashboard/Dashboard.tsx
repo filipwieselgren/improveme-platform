025e4fe085aadfedc35a52f1e0a15d3f154783ab
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2023-01-19 22:17:52 +0100

    Dashboard statistik

--- src/pages/loggedin/dashboard/Dashboard.tsx
+++ src/pages/loggedin/dashboard/Dashboard.tsx
@@ -10,7 +10,7 @@ import { IBugReport } from "../../../models/IBugReport";
 import IGeneralImprovements from "../../../models/IGeneralImprovements";
 import { IGetParts } from "../../../models/IPart";
 import { Bar } from "react-chartjs-2";
-import { Chart as ChartJS } from "chart.js/auto";
+import Chart, { Chart as ChartJS, ChartType } from "chart.js/auto";
 
 interface IDashboard {
   errend: IErrends;
@@ -22,7 +22,15 @@ const Dashboard = (props: IDashboard) => {
   const [fixedBugReports, setFixedBugReports] = useState<number>(0);
   const [fixedGeneralImprovements, setFixedGeneralImprovements] =
     useState<number>(0);
+  const [allFeaturesRequests, setAllFeaturesRequests] = useState<number>(0);
+  const [allBugReports, setAllBugReports] = useState<number>(0);
+  const [allGeneralImprovements, setAllGeneralImprovements] =
+    useState<number>(0);
+  const [frAmout, setFrAmout] = useState<number>(0);
+  const [brAmout, setBrAmout] = useState<number>(0);
+  const [giAmout, setGiAmout] = useState<number>(0);
 
+  const [allRequests, setAllRequests] = useState<number>(0);
   const [displayName, setDisplayName] = useState({
     auth: true,
     token: "",
@@ -30,7 +38,7 @@ const Dashboard = (props: IDashboard) => {
   });
 
   useEffect(() => {
-    // Count fixed Features Requests
+    // Count Done Features Requests
     const getFixedFeaturesRequests = props.errend.getFeatureRequests.filter(
       (fr: IFeatureRequest) => fr.status === "Done"
     );
@@ -38,7 +46,7 @@ const Dashboard = (props: IDashboard) => {
       props.errend.getCountFeatureRequests
     );
 
-    // Count fixed Bug Reports
+    // Count Done Bug Reports
 
     const getFixedBugReports = props.errend.getBugReports.filter(
       (br: IBugReport) => br.status === "Done"
@@ -48,7 +56,7 @@ const Dashboard = (props: IDashboard) => {
       props.errend.getCountBugReports
     );
 
-    // Count fixed General Improvements
+    // Count Done General Improvements
 
     const getFixedGeneralImprovements =
       props.errend.getGeneralImprovements.filter(
@@ -67,7 +75,56 @@ const Dashboard = (props: IDashboard) => {
     props.errend.getFeatureRequests,
     props.errend.getGeneralImprovements,
     props.errend.getCountFeatureRequests,
+    props.errend.getCountGeneralImprovements,
+    props.errend.getCountBugReports,
   ]);
+
+  useEffect(() => {
+    const allFeaturesRequests = props.errend.getFeatureRequests;
+
+    const allDoneFeaturesRequests = allFeaturesRequests.concat(
+      props.errend.getCountFeatureRequests
+    );
+
+    const allBugReports = props.errend.getBugReports;
+
+    const allDoneBugReports = allBugReports.concat(
+      props.errend.getCountBugReports
+    );
+
+    const allGeneralImprovements = props.errend.getGeneralImprovements;
+
+    const allDoneGeneralImprovements = allGeneralImprovements.concat(
+      props.errend.getCountGeneralImprovements
+    );
+
+    setAllFeaturesRequests(allDoneFeaturesRequests.length);
+    setAllBugReports(allDoneBugReports.length);
+    setAllGeneralImprovements(allDoneGeneralImprovements.length);
+  }, [
+    props.errend.getBugReports,
+    props.errend.getFeatureRequests,
+    props.errend.getGeneralImprovements,
+    props.errend.getCountFeatureRequests,
+    props.errend.getCountGeneralImprovements,
+    props.errend.getCountBugReports,
+  ]);
+
+  useEffect(() => {
+    const allRequests =
+      allFeaturesRequests + allBugReports + allGeneralImprovements;
+
+    const frProcent = (allFeaturesRequests / allRequests) * 100;
+    const brProcent = (allBugReports / allRequests) * 100;
+    const giProcent = (allGeneralImprovements / allRequests) * 100;
+
+    setFrAmout(Math.round(frProcent));
+    setBrAmout(Math.round(brProcent));
+    setGiAmout(Math.round(giProcent));
+    setAllRequests(allRequests);
+  }, [allFeaturesRequests]);
+
+  console.log("frAmout:", frAmout);
 
   return (
     <div className="dashboard-wrapper">
@@ -100,6 +157,30 @@ const Dashboard = (props: IDashboard) => {
             title={fixedBugReports}
             info={fixedBugReports === 1 ? "Bug Report" : "Bug Reports"}
           />
+        </div>
+
+        <div className="bars-full-wrapper">
+          <div className="bars-title-wrapper">
+            <h4 className="bars-title">Distribution of requests</h4>
+            <div className="box-wrapper">
+              <div className="fr-box box "></div> <span>Feature Requests</span>
+              <div className="gi-box box"></div>{" "}
+              <span>General Improvements</span>
+              <div className="br-box box"></div> <span>Bug Reports</span>
+            </div>
+          </div>
+          <div className="bars-wrapper">
+            <div className="bar fr-bar" style={{ height: `${frAmout}%` }}>
+              {Number.isNaN(frAmout) ? "0 %" : `${frAmout} %`}
+            </div>
+            <div className="bar gi-bar" style={{ height: `${brAmout}%` }}>
+              {Number.isNaN(brAmout) ? "0 %" : `${brAmout} %`}
+            </div>
+            <div className="bar br-bar" style={{ height: `${giAmout}%` }}>
+              {Number.isNaN(giAmout) ? "0 %" : `${giAmout} %`}
+            </div>
+          </div>
+          <div className="total-requests">Total requests: {allRequests}</div>
         </div>
       </div>
     </div>
