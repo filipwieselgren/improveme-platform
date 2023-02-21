ca44ba51cab55157fe6d174aab0d09aa4268825e
filipwieselgren <91126634+filipwieselgren@users.noreply.github.com>
2023-02-21 22:02:12 +0100

    db key

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const app = require("./app");
 
-const DBKEY = process.env.MONGODBKEY;
+const DBKEY = process.env.MONGODB_URI;
 
 mongoose
   .connect(DBKEY, { useNewUrlParser: true, useUnifiedTopology: true })
